db2ca83045c3ca34b6b05d7e20391576ba34e7f3
tbu222 <thinhbui2911@gmail.com>
2022-11-03 08:13:58 -0700

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+//ES2015 Global Constants
+const PI = 3.14;
+
+// What is the difference between var and let?
+// var allow redeclare while let does not.
+
+// What is the difference between var and const?
+// var allow redeclare while const does not
+
+// What is the difference between let and const?
+// let allow reassign while const does not
+
+// What is hoisting?
+// Hoisting is JavaScript's default behavior of moving all declarations to the top of the current scope
